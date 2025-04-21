45e25acb346216a40d2986be51ad729bb9d1383f
TakeshiSugimori <bigblog090@gmail.com>
2025-04-21 21:44:22 +0900

    githubpagesの準備

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // ...他の設定...
+  site: 'https://takeshi-du.github.io', // ← サイトのドメイン部分も設定推奨
+  base: '/astro-tutorial', // ← この行を追加/修正
+});
